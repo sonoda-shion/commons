4aadba5aeeed1c7cc0e029abf81db46bbb0af541
sonoda-shion <xcgikdue2z@i.softbank.jp>
2022-11-24 14:22:01 +0900

    js-nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,15 @@
-
+$(function () {
+    $("#js-btn").on("click", function () {
+      $(".js-menu-btn").toggleClass("active");
+      $("#js-nav").toggleClass("show");
+      $('body').toggleClass('open');
+      $('.nav__link-left').toggleClass('fadeUp-list-left');
+      $('.nav__link-right').toggleClass('fadeUp-list-right');
+    });
+    $(".cross").on("click",function() {
+        $("#js-nav").removeClass("show");
+        $('body').removeClass('open');
+    })
+});
+  
 
